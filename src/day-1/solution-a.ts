ca5aea8aa7bf2349e595faffa998ccc6029addba
afsardo <afsardo@gmail.com>
2022-12-01 13:02:44 +0000

    solution: Day 1

--- src/day-1/solution-a.ts
+++ src/day-1/solution-a.ts
@@ -1,3 +1,15 @@
 export const solution = "a";
 
 const input = await Bun.file("src/day-1/input.txt").text();
+
+const elfs = [];
+let currentCalories = 0;
+for (const line of input.split("\n")) {
+  if (line === "") {
+    elfs.push(currentCalories);
+    currentCalories = 0;
+  } else {
+    currentCalories += Number(line);
+  }
+}
+console.log(elfs.sort((a, b) => a - b).reverse().at(0));
--- src/day-1/solution-b.ts
+++ src/day-1/solution-b.ts
@@ -1,3 +1,15 @@
 export const solution = "b";
 
 const input = await Bun.file("src/day-1/input.txt").text();
+
+const elfs = [];
+let currentCalories = 0;
+for (const line of input.split("\n")) {
+  if (line === "") {
+    elfs.push(currentCalories);
+    currentCalories = 0;
+  } else {
+    currentCalories += Number(line);
+  }
+}
+console.log(elfs.sort((a, b) => a - b).reverse().slice(0, 3).reduce((a, b) => a + b, 0));
