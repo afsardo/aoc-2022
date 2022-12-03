fa445652fcf628c8db54c2c5bcee7ba75300a600
afsardo <afsardo@gmail.com>
2022-12-03 13:42:32 +0000

    solution: Day 3

--- src/day-3/solution-a.ts
+++ src/day-3/solution-a.ts
@@ -1,3 +1,23 @@
 export const solution = "a";
 
 const input = await Bun.file("src/day-3/input.txt").text();
+const alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+let sum = 0;
+
+for (const line of input.split("\n")) {
+  const items = line.split("");
+  const sideA = items.slice(0, items.length / 2);
+  const sideB = items.slice(items.length / 2);
+  const alreadyFound = [];
+
+  for (const item of sideA) {
+    if (sideB.includes(item) && !alreadyFound.includes(item)) {
+      alreadyFound.push(item);
+      sum += alphabet.indexOf(item) + 1;
+    }
+  }
+
+}
+
+console.log("Solution:", sum);
--- src/day-3/solution-b.ts
+++ src/day-3/solution-b.ts
@@ -1,4 +1,35 @@
 export const solution = "b";
 
 const input = await Bun.file("src/day-3/input.txt").text();
+const alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+
+let sum = 0;
+
+const lines = input.split("\n");
+for (let i = 0; i < lines.length; i += 3) {
+  if (lines.length < i + 3) {
+    break;
+  }
+
+  const lineA = lines[i];
+  const lineB = lines[i + 1];
+  const lineC = lines[i + 2];
+
+  const match = [];
+  for (const item of lineB) {
+    if (lineA.includes(item) && !match.includes(item)) {
+      match.push(item);
+    }
+  }
+
+  for (const item of lineC) {
+    if (match.includes(item)) {
+      sum += alphabet.indexOf(item) + 1;
+      break;
+    }
+  }
+}
+
+console.log("Solution:", sum);
 
