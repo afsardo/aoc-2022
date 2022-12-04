356c8f847f60a852baded962fee4774e75b0ef94
afsardo <afsardo@gmail.com>
2022-12-04 09:18:13 +0000

    solution: Day 4

--- src/day-4/solution-a.ts
+++ src/day-4/solution-a.ts
@@ -1,3 +1,23 @@
 export const solution = "a";
 
 const input = await Bun.file("src/day-4/input.txt").text();
+
+let count = 0;
+for (const line of input.split("\n")) {
+  const [pairA, pairB] = line.split(",");
+  const [pairAStartStr, pairAEndStr] = pairA.split("-");
+  const pairAStart = parseInt(pairAStartStr, 10);
+  const pairAEnd = parseInt(pairAEndStr, 10);
+  const [pairBStartStr, pairBEndStr] = pairB.split("-");
+  const pairBStart = parseInt(pairBStartStr, 10);
+  const pairBEnd = parseInt(pairBEndStr, 10);
+
+  if (
+    (pairAStart >= pairBStart && pairAEnd <= pairBEnd) ||
+    (pairBStart >= pairAStart && pairBEnd <= pairAEnd)
+  ) {
+    count++;
+  }
+}
+
+console.log("Solution:", count);
--- src/day-4/solution-b.ts
+++ src/day-4/solution-b.ts
@@ -2,3 +2,24 @@ export const solution = "b";
 
 const input = await Bun.file("src/day-4/input.txt").text();
 
+let count = 0;
+for (const line of input.split("\n")) {
+  const [pairA, pairB] = line.split(",");
+  const [pairAStartStr, pairAEndStr] = pairA.split("-");
+  const pairAStart = parseInt(pairAStartStr, 10);
+  const pairAEnd = parseInt(pairAEndStr, 10);
+  const [pairBStartStr, pairBEndStr] = pairB.split("-");
+  const pairBStart = parseInt(pairBStartStr, 10);
+  const pairBEnd = parseInt(pairBEndStr, 10);
+
+  if (
+    (pairAStart >= pairBStart && pairAEnd <= pairBEnd) ||
+    (pairAStart <= pairBStart && pairAStart <= pairBEnd && pairAEnd >= pairBStart && pairAEnd <= pairBEnd) ||
+    (pairBStart >= pairAStart && pairBEnd <= pairAEnd) ||
+    (pairBStart <= pairAStart && pairBStart <= pairAEnd && pairBEnd >= pairAStart && pairBEnd <= pairAEnd)
+  ) {
+    count++;
+  }
+}
+
+console.log("Solution:", count);
