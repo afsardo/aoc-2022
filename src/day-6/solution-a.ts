9a5149fcb2fede94296f0299c9c50d615e3fc67c
afsardo <afsardo@gmail.com>
2022-12-06 15:37:15 +0000

    solution: Day 6

--- src/day-6/solution-a.ts
+++ src/day-6/solution-a.ts
@@ -1,3 +1,25 @@
 export const solution = "a";
 
 const input = await Bun.file("src/day-6/input.txt").text();
+
+const last4Bits = [];
+const chars = input.split("");
+let startPacketIndex = -1;
+for (let i = 0; i < chars.length; i++) {
+  const char = chars[i];
+
+  if (last4Bits.length < 4) {
+    last4Bits.push(char);
+    continue;
+  }
+
+  last4Bits.shift();
+  last4Bits.push(char);
+  const uniqueBits = new Set(last4Bits);
+  if (uniqueBits.size === last4Bits.length) {
+    startPacketIndex = i;
+    break;
+  }
+}
+
+console.log("Solution:", startPacketIndex +1)
--- src/day-6/solution-b.ts
+++ src/day-6/solution-b.ts
@@ -2,3 +2,24 @@ export const solution = "b";
 
 const input = await Bun.file("src/day-6/input.txt").text();
 
+const last4Bits = [];
+const chars = input.split("");
+let startPacketIndex = -1;
+for (let i = 0; i < chars.length; i++) {
+  const char = chars[i];
+
+  if (last4Bits.length < 14) {
+    last4Bits.push(char);
+    continue;
+  }
+
+  last4Bits.shift();
+  last4Bits.push(char);
+  const uniqueBits = new Set(last4Bits);
+  if (uniqueBits.size === last4Bits.length) {
+    startPacketIndex = i;
+    break;
+  }
+}
+
+console.log("Solution:", startPacketIndex +1)
