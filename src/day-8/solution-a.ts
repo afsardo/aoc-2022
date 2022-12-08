fb0bd66b3da3ed751234b3e5dc4506c96ca948e2
afsardo <afsardo@gmail.com>
2022-12-08 12:15:12 +0000

    solution: Day 8

--- src/day-8/solution-a.ts
+++ src/day-8/solution-a.ts
@@ -1,3 +1,60 @@
 export const solution = "a";
 
 const input = await Bun.file("src/day-8/input.txt").text();
+
+const matrix : number[][] = [];
+
+for (const line of input.split("\n")) {
+  const row = line.split("").map((x) => parseInt(x));
+  matrix.push(row);
+}
+
+function isTreeVisible(x: number, y: number, matrix: number[][]) {
+  const width = matrix[0].length;
+  const height = matrix.length;
+  
+  let isVisbleFromTop = true;
+  for (let j = y - 1; j >= 0; j--) {
+    if (matrix[x][j] >= matrix[x][y]) {
+      isVisbleFromTop = false;
+      break;
+    }
+  }
+
+  let isVisbleFromBottom = true;
+  for (let j = y + 1; j < height; j++) {
+    if (matrix[x][j] >= matrix[x][y]) {
+      isVisbleFromBottom = false;
+      break;
+    }
+  }
+
+  let isVisbleFromLeft = true;
+  for (let i = x - 1; i >= 0; i--) {
+    if (matrix[i][y] >= matrix[x][y]) {
+      isVisbleFromLeft = false;
+      break;
+    }
+  }
+
+  let isVisbleFromRight = true;
+  for (let i = x + 1; i < width; i++) {
+    if (matrix[i][y] >= matrix[x][y]) {
+      isVisbleFromRight = false;
+      break;
+    }
+  }
+
+  return isVisbleFromTop || isVisbleFromBottom || isVisbleFromLeft || isVisbleFromRight;
+}
+
+let visibleTrees = 0;
+for (let i = 0; i < matrix.length; i++) {
+  for (let j = 0; j < matrix[i].length; j++) {
+    if (isTreeVisible(i, j, matrix)) {
+      visibleTrees++;
+    }
+  }
+}
+
+console.log("Solution:", visibleTrees);
--- src/day-8/solution-b.ts
+++ src/day-8/solution-b.ts
@@ -2,3 +2,60 @@ export const solution = "b";
 
 const input = await Bun.file("src/day-8/input.txt").text();
 
+const matrix : number[][] = [];
+
+for (const line of input.split("\n")) {
+  const row = line.split("").map((x) => parseInt(x));
+  matrix.push(row);
+}
+
+function calculateScenicScore(x: number, y: number, matrix: number[][]) {
+  const width = matrix[0].length;
+  const height = matrix.length;
+  
+  let visibleTreesFromTop = 0;
+  for (let j = y - 1; j >= 0; j--) {
+    visibleTreesFromTop++;
+    if (matrix[x][j] >= matrix[x][y]) {
+      break;
+    }
+  }
+
+  let visibleTreesFromBottom = 0;
+  for (let j = y + 1; j < height; j++) {
+    visibleTreesFromBottom++;
+    if (matrix[x][j] >= matrix[x][y]) {
+      break;
+    }
+  }
+
+  let visibleTreesFromLeft = 0;
+  for (let i = x - 1; i >= 0; i--) {
+    visibleTreesFromLeft++;
+    if (matrix[i][y] >= matrix[x][y]) {
+      break;
+    }
+  }
+
+  let visibleTreesFromRight = 0;
+  for (let i = x + 1; i < width; i++) {
+    visibleTreesFromRight++;
+    if (matrix[i][y] >= matrix[x][y]) {
+      break;
+    }
+  }
+  
+  return visibleTreesFromTop * visibleTreesFromBottom * visibleTreesFromLeft * visibleTreesFromRight;
+}
+
+let highestScore = -1;
+for (let i = 0; i < matrix.length; i++) {
+  for (let j = 0; j < matrix[i].length; j++) {
+    const score = calculateScenicScore(i, j, matrix);
+    if (score > highestScore) {
+      highestScore = score;
+    }
+  }
+}
+
+console.log("Solution:", highestScore);
