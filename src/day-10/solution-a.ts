efd33cd8f76c25a3fa04385c8f5750ba7dbd7167
afsardo <afsardo@gmail.com>
2022-12-10 20:09:11 +0000

    solution: Day 10

--- src/day-10/solution-a.ts
+++ src/day-10/solution-a.ts
@@ -1,3 +1,37 @@
 export const solution = "a";
 
 const input = await Bun.file("src/day-10/input.txt").text();
+
+let x = 1;
+let cycles = 0;
+const signalStrengths = {};
+
+for (const line of input.split("\n")) {
+  const [command, value] = line.split(" ");
+
+  if (command === "addx") {
+    cycles += 1;
+  }
+
+  if (command === "noop") {
+    cycles += 1;
+  }
+
+  if (cycles === 20 || (cycles + 20) % 40 === 0) {
+    signalStrengths[cycles] = cycles * x;
+  }
+
+  if (command === "addx") {
+    cycles += 1;
+  }
+  
+  if (cycles === 20 || (cycles + 20) % 40 === 0) {
+    signalStrengths[cycles] = cycles * x;
+  }
+
+  if (command === "addx") {
+    x += parseInt(value);
+  }
+}
+
+console.log("Solution:", Object.values(signalStrengths).reduce((a:number, b:number) => a + b, 0))
--- src/day-10/solution-b.ts
+++ src/day-10/solution-b.ts
@@ -2,3 +2,39 @@ export const solution = "b";
 
 const input = await Bun.file("src/day-10/input.txt").text();
 
+let x = 1;
+let cycles = 0;
+let rowPixels = "";
+
+for (const line of input.split("\n")) {
+  const [command, value] = line.split(" ");
+
+  if (command === "addx") {
+    cycles += 1;
+  }
+
+  if (command === "noop") {
+    cycles += 1;
+  }
+
+  rowPixels += cycles % 40 > x - 1 && cycles % 40 < x + 3 ? "#" : ".";
+
+  if (cycles % 40 === 0 && rowPixels.length === 40) {
+    console.log(rowPixels);
+    rowPixels = "";
+  }
+
+  if (command === "addx") {
+    cycles += 1;
+    rowPixels += cycles % 40 > x - 1 && cycles % 40 < x + 3 ? "#" : ".";
+  }
+  
+  if (cycles % 40 === 0 && rowPixels.length === 40) {
+    console.log(rowPixels);
+    rowPixels = "";
+  }
+
+  if (command === "addx") {
+    x += parseInt(value);
+  }
+}
