5c559016fab0e79762d60ba1981b31200d0c7c1e
afsardo <afsardo@gmail.com>
2022-12-13 12:54:20 +0000

    solution: Day 13

--- src/day-13/solution-a.ts
+++ src/day-13/solution-a.ts
@@ -1,3 +1,61 @@
 export const solution = "a";
 
 const input = await Bun.file("src/day-13/input.txt").text();
+
+const packetPairs = input.split("\n\n").map((pair) => {
+  const [leftPacket, rightPacket] = pair.split("\n");
+  return [
+    JSON.parse(leftPacket),
+      JSON.parse(rightPacket),
+  ]
+});
+
+function correctOrder(leftPacket, rightPacket) {
+  for (let i = 0; i < leftPacket.length; i++) {
+    if (rightPacket.length <= i) {
+      return false;
+    }
+
+    let leftElement = leftPacket[i];
+    let rightElement = rightPacket[i];
+
+    if (typeof leftElement === "number" && typeof rightElement === "number") {
+      if (leftElement < rightElement) {
+        return true;
+      }
+      if (leftElement > rightElement) {
+        return false;
+      }
+    } else {
+      if (typeof leftElement === "number") {
+        leftElement = [leftElement];
+      }
+      if (typeof rightElement === "number") {
+        rightElement = [rightElement];
+      }
+
+      const correctOrderResult = correctOrder(leftElement, rightElement);
+      if (correctOrderResult !== null) {
+        return correctOrderResult;
+      }
+    }
+  }
+
+  if (leftPacket.length < rightPacket.length) {
+    return true;
+  }
+
+  return null;
+}
+
+const correctPacketPairsIndexes = [];
+for (let i = 0; i < packetPairs.length; i++) {
+  const [leftPacket, rightPacket] = packetPairs[i];
+
+
+  if (correctOrder(leftPacket, rightPacket)) {
+    correctPacketPairsIndexes.push(i + 1);
+  }
+}
+
+console.log("Solution:", correctPacketPairsIndexes.reduce((a, b) => a + b, 0));
--- src/day-13/solution-b.ts
+++ src/day-13/solution-b.ts
@@ -2,3 +2,63 @@ export const solution = "b";
 
 const input = await Bun.file("src/day-13/input.txt").text();
 
+const packets = input.split("\n").map((packet) => {
+  if (!packet) return false;
+  return JSON.parse(packet)
+}).filter(Boolean);
+
+const firstDividerPacket = [[2]];
+packets.push(firstDividerPacket);
+
+const secondDividerPacket = [[6]];
+packets.push(secondDividerPacket);
+
+function compare(leftPacket, rightPacket) {
+  for (let i = 0; i < leftPacket.length; i++) {
+    if (rightPacket.length <= i) {
+      return 1;
+    }
+
+    let leftElement = leftPacket[i];
+    let rightElement = rightPacket[i];
+
+    if (typeof leftElement === "number" && typeof rightElement === "number") {
+      if (leftElement < rightElement) {
+        return -1;
+      }
+      if (leftElement > rightElement) {
+        return 1;
+      }
+    } else {
+      if (typeof leftElement === "number") {
+        leftElement = [leftElement];
+      }
+      if (typeof rightElement === "number") {
+        rightElement = [rightElement];
+      }
+
+      const compareResult = compare(leftElement, rightElement);
+      if (compareResult !== 0) {
+        return compareResult;
+      }
+    }
+  }
+
+  if (leftPacket.length < rightPacket.length) {
+    return -1;
+  }
+
+  return 0;
+}
+
+const sortedPackets = packets.sort(compare);
+
+const firstDividerPacketIndex = sortedPackets.findIndex((packet) => {
+  return firstDividerPacket == packet;
+}) + 1;
+
+const secondDividerPacketIndex = sortedPackets.findIndex((packet) => {
+  return secondDividerPacket == packet;
+}) + 1;
+
+console.log("Solution:", firstDividerPacketIndex * secondDividerPacketIndex);
